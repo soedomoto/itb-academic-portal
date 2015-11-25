3899b5c16fa8e8c210401ed462150b63a147fcb5
soedomoto <soedomoto@gmail.com>
2015-11-25 21:25:59 +0700

    Add dependencies and build tools

--- src/main/java/id/ac/itb/academic/service/HelloService.java
+++ src/main/java/id/ac/itb/academic/service/HelloService.java
@@ -1,14 +1,26 @@
 package id.ac.itb.academic.service;
 
+import java.util.Date;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("hello")
 public class HelloService {
 	
 	@GET
+	@Path("world")
 	public String hello() {
 		return "Hello World";
+	}
+	
+	@GET
+	@Path("time")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Date now() {
+		return new Date();
 	}
 
 }
